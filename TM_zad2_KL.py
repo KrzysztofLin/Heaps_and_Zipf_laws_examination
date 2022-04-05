aa039cf537506e000a63ef4cfec29a975efc9399
Krzychiu <krzysztof.lingoo@gmail.com>
2022-04-05 17:31:33 +0200

    git add Heaps.py

--- TM_zad2_KL.py
+++ TM_zad2_KL.py
@@ -5,7 +5,6 @@ from collections import Counter
 import pandas as pd
 import matplotlib.pyplot as plt
 from math import log
-from Heaps import HeapsZipfLaws
 
 def get_files(path: str) -> List[str]:
     files_names = []
@@ -24,7 +23,7 @@ def read_text_file(file_path: str) -> str:
     return readed_file
 
 
-def data_harvester(files_names, files_content: List[str]) -> List[int]:
+def data_harvester(files_names, files_content: List[str]) -> tuple[List[int], List[int]]:
     file_dict = {}
     token_number_list = []
     terms_number_list = []
@@ -32,6 +31,7 @@ def data_harvester(files_names, files_content: List[str]) -> List[int]:
         preprocesed_tokens = preproces_data(raw_text)
         counted_tokens = Counter(preprocesed_tokens)
         file_dict[file_name] = counted_tokens
+
         if token_number_list != []:
             terms_number_list.append(len(counted_tokens))
             token_number_list.append(len(preprocesed_tokens))
@@ -39,32 +39,14 @@ def data_harvester(files_names, files_content: List[str]) -> List[int]:
         terms_number_list.append(len(df.columns))
         token_number_list.append(df.values.sum())
 
-    terms_token_number_list = [sorted(terms_number_list), sorted(token_number_list)]
-    return df, terms_token_number_list
-
-
-def Heaps_calculation(terms_token_list: List[int]) -> int:
-    terms_tokens_number = [[6042, 17843, 23885.0, 24609, 25364, 34126, 58011.0, 83375.0, 107984.0],
-                           [2135, 2558, 3697, 3980, 4196, 5207, 6848, 8211, 9256]]
-    #for M, T in zip(terms_tokens_number[1], terms_token_number[0]):
-    #log(M) = b * log(T) + logk
-
-
-    #log(M) - log(M2) = b * (logT - logT2)
+    return df, sorted(terms_number_list), sorted(token_number_list)
 
 
 def main(path: str):
-
     files_names, files_content = get_files(path)
-    df, terms_token_number_list = data_harvester(files_names, files_content)
-    print(terms_token_number_list)
-    HeapsZipfLaws.heaps_data_graph(terms_token_number_list)
-    plt.plot(terms_token_number_list[1], terms_token_number_list[0])
-    plt.show()
+    df, terms_number_list, token_number_list = data_harvester(files_names, files_content)
+
     df.to_csv('genrated_data.csv', header=True, index=True,mode='w')
-
-
-
 
 
 if __name__ == "__main__":
