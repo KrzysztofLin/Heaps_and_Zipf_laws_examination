e3c4665579fa5b74290e970f650ac6b2fc7de451
Krzychiu <krzysztof.lingoo@gmail.com>
2022-04-06 08:37:52 +0200

    git add Heaps.py

--- TM_zad2_KL.py
+++ TM_zad2_KL.py
@@ -3,16 +3,17 @@ from typing import List, Dict
 from preproces_data import preproces_data
 from collections import Counter
 import pandas as pd
-import matplotlib.pyplot as plt
-from math import log
 
-def get_files(path: str) -> List[str]:
+PATH = "C:\\Users\\Krzychiu\\Documents\\Analiza_danych_studia\\2 semestr\\Text Mining\\Text_Minning"
+
+
+def get_files(PATH: str) -> tuple[List[str], List[str]]:
     files_names = []
     files_content = []
     for file_name in os.listdir():
         if file_name.endswith(".txt"):
             files_names.append(file_name)
-            file_path = f"{path}\{file_name}"
+            file_path = f"{PATH}\{file_name}"
             files_content.append(read_text_file(file_path))
     return files_names, files_content
 
@@ -23,7 +24,7 @@ def read_text_file(file_path: str) -> str:
     return readed_file
 
 
-def data_harvester(files_names, files_content: List[str]) -> tuple[List[int], List[int]]:
+def data_harvester(files_names, files_content: List[str]) -> tuple[pd.DataFrame, List[int], List[int]]:
     file_dict = {}
     token_number_list = []
     terms_number_list = []
@@ -31,24 +32,24 @@ def data_harvester(files_names, files_content: List[str]) -> tuple[List[int], Li
         preprocesed_tokens = preproces_data(raw_text)
         counted_tokens = Counter(preprocesed_tokens)
         file_dict[file_name] = counted_tokens
-
         if token_number_list != []:
             terms_number_list.append(len(counted_tokens))
             token_number_list.append(len(preprocesed_tokens))
         df = pd.DataFrame(data=file_dict).T.fillna(0)
         terms_number_list.append(len(df.columns))
         token_number_list.append(df.values.sum())
-
     return df, sorted(terms_number_list), sorted(token_number_list)
 
 
-def main(path: str):
-    files_names, files_content = get_files(path)
+def main(PATH: str):
+    files_names, files_content = get_files(PATH)
     df, terms_number_list, token_number_list = data_harvester(files_names, files_content)
+    df.to_csv('genrated_data.csv', header=True, index=True, mode='w') #you can look through document
 
-    df.to_csv('genrated_data.csv', header=True, index=True,mode='w')
+    #Zipf law
+
+    frequency_dict = df.to_dict('records')
 
 
 if __name__ == "__main__":
-    path = "C:\\Users\\Krzychiu\\Documents\\Analiza_danych_studia\\2 semestr\\Text Mining\\Zad_dom_2"
-    main(path)
+    main(PATH)
